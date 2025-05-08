6073d3d86e61f1d777404a0aa985e39fdab2ec19
“perl-0815” <chico.poiyo@gmail.com>
2025-05-09 01:50:27 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
-import withPWA from 'next-pwa'
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+  /* config options here */
+};
 
-export default withPWA({
-  ...nextConfig,
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development', // 開発中は無効
-  },
-})
+export default nextConfig;
--- src/types/next-pwa.d.ts
+++
@@ -1,2 +0,0 @@
-// types/next-pwa.d.ts
-declare module 'next-pwa';
